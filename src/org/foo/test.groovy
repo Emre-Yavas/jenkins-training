be0a6c2b2513f604ff57431df62fe625ff8d9c03
Emre Yavaş <69636329+Emre-Yavas@users.noreply.github.com>
2023-03-06 11:19:10 +0300

    Update test.groovy

--- src/org/foo/test.groovy
+++ src/org/foo/test.groovy
@@ -2,7 +2,7 @@ package org.foo
 
 public class test {
      
-     private static Map<String, Integer> eventMap = new HashMap<>();
+     private static Map<String, Integer> eventMap = new HashMap<String, Integer>();
      eventMap.put('Emre',1);
      
      println(eventMap);
