933230377c8da1e218bad416f7cbce7ff9e7fd5a
Emre Yavaş <69636329+Emre-Yavas@users.noreply.github.com>
2023-03-30 20:12:25 +0300

    Update test.groovy

--- src/org/foo/test.groovy
+++ src/org/foo/test.groovy
@@ -1,9 +1,5 @@
 package org.foo
 
-public class test {
-     
-     private static Map<Integer, Integer> eventMap = new HashMap<String, Integer>();
-     eventMap.put(3, 1);
-     
-     println(eventMap);
+public class Test {
+     static String foo = "foo"    
 }
