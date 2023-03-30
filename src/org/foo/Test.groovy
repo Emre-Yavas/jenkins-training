d7c7730f689670ee1aa399aee11a7b7466c2143b
Emre Yavaş <69636329+Emre-Yavas@users.noreply.github.com>
2023-03-30 20:29:51 +0300

    Update Test.groovy

--- src/org/foo/Test.groovy
+++ src/org/foo/Test.groovy
@@ -2,6 +2,6 @@ package org.foo
 
 public class Test  {
      public static String publish(String eventName = "Raw"){
-          return 'Bu event ${eventName} !'
+          return "Bu event ${eventName} !"
      }
 }
